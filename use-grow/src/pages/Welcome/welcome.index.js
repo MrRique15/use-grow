d697510b42ea635ad8917eba13bbf88a086b91e1
MrRique15 <henrique-favaro@hotmail.com>
2022-08-25 15:46:23 -0300

    First Pages

---
+++ use-grow/src/pages/Welcome/welcome.index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text,StyleSheet } from "react-native";
+
+export default function Welcome() {
+  return( 
+    <View>
+        <Text>Welcome to use-grow</Text>
+    </View>
+  );
+}
---
+++ use-grow/src/routes/index.js
@@ -0,0 +1,28 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Welcome from '../pages/Welcome';
+import SignIn from '../pages/Signin';
+
+const Stack = createNativeStackNavigator();
+
+export default function Routes(){
+    return(
+        <Stack.Navigator>
+            <Stack.Screen 
+                name="Home" 
+                component={Welcome} 
+                options={{
+                    headerShown: false
+                }}
+            />
+
+            <Stack.Screen
+                name="SignIn"
+                component={SignIn}
+                options={{
+                    headerShown: false
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
